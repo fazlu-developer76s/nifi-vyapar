3127faad3e7f3ac295d18a7bba4e11cd9e968ebf
Fazlu Rehman <fazlu.developer@gmail.com>
2025-07-01 16:11:33 +0530

    set

--- src/utils/googleConfig.js
+++ src/utils/googleConfig.js
@@ -1,4 +1,6 @@
 import { google } from "googleapis"
+import dotenv from "dotenv";
+dotenv.config();
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
